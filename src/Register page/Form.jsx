bbcf6ac9968bf389824a9f8e1cdc6a6fe74752ec
TommyDev69 <sunnevian4life@gmail.com>
2026-03-08 19:58:52 +0100

    update

--- src/Register page/Form.jsx
+++ src/Register page/Form.jsx
@@ -178,7 +178,9 @@ const Form = ({
             </button>
           </div>
           <div className="flex">
-
+              <div>
+                <input type="checkbox" id="terms" name="terms" className="mr-2" />
+              </div>
           <div className="flex md:mx-auto texvt-cednter   items-center justify-center md:text-[12px] text-[9px]  md:w-[375px] mt-2">
             <p className='md:w-full w-8/12 text-center'>
               By continuing you agree you have read, understood and agree Terms and Conditions and Privacy Policy
